52b76631fdef21daf96f218ed77db580a2096d8a
DESKTOP-6KBPV3D\user <jon00141@naver.com>
2023-06-30 21:46:03 +0900

    [여진]  - database 연동 연습문제1 진행중

---
+++ src/main/java/com/marondal/database/test/Test01Controller.java
@@ -0,0 +1,47 @@
+package com.marondal.database.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.marondal.common.MysqlService;
+
+public class Test01Controller extends HttpServlet {
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setContentType("text/plain");
+		PrintWriter out = response.getWriter();
+		
+		MysqlService mysqlService = MysqlService.getInstance(); 
+		//접속
+		mysqlService.connect();
+		String selectQuery = "SELECT * FROM `real_estate`;";
+		ResultSet resultSet = mysqlService.select(selectQuery);
+		
+		String insertQuery = "INSERT INTO `real_estate`\r\n"
+				+ "(`realtorId`, `address`, `area`, `type`, `price`, `rentPrice`)\r\n"
+				+ "VALUE \r\n"
+				+ "(3, '헤라펠리스 101동 5305호', 350, '매매', 1500000, NULL);";
+		
+		int count = mysqlService.update(insertQuery);
+		
+		try {
+			while(resultSet.next() ) {
+				String address = resultSet.getString("address");
+				String area = resultSet.getString("area");
+				String type = resultSet.getString("type");
+				
+				out.println("매물주소 : " + address + ", 면적 : " + area + ", 타입 : " + type);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	
+		
+	}
+}
